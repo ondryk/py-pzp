01231ab4bffb01c95af053d7242e5640fe58033a
Ondrej Berger <berger.ondrej@gmail.com>
2024-10-24 05:39:38 +0200

    Add state value prefixes for openmetric converter

--- pzp/pages/states.py
+++ pzp/pages/states.py
@@ -9,14 +9,14 @@ class RunningStateParser(PageParser):
     def parse(self, text: str):
         result = []
         try:
-            result.append(self.parse_bool("Běh kompresoru", "T7E3FC003"))
-            result.append(self.parse_bool("Oběhové čerpadlo topení 1", "T1F74EFF9"))
-            result.append(self.parse_bool("Ohřev TUV", "TB468BAE2"))
-            result.append(self.parse_bool("Přímotopný ohřev TUV", "TB7060E15"))
-            result.append(self.parse_bool("Provoz primárního okruhu", "T43337318"))
-            result.append(self.parse_bool("Oběhové čerpadlo sekundárního okruhu", "T782CD2B5"))
-            result.append(self.parse_bool("Běh elektrokotle č.1", "T8E22B3AD"))
-            result.append(self.parse_bool("Běh elektrokotle č.2", "T95CF3693"))
+            result.append(self.parse_bool("S01: Běh kompresoru", "T7E3FC003"))
+            result.append(self.parse_bool("S02: Oběhové čerpadlo topení č.4", "T1F74EFF9"))
+            result.append(self.parse_bool("S03: Ohřev TUV", "TB468BAE2"))
+            result.append(self.parse_bool("S04: Přímotopný ohřev TUV", "TB7060E15"))
+            result.append(self.parse_bool("S05: Provoz primárního okruhu", "T43337318"))
+            result.append(self.parse_bool("S06: Oběhové čerpadlo sekundárního okruhu", "T782CD2B5"))
+            result.append(self.parse_bool("S07: Běh elektrokotle č.1", "T8E22B3AD"))
+            result.append(self.parse_bool("S08: Běh elektrokotle č.2", "T95CF3693"))
         except Exception as e:
             print(f"Error during parsing: {e}")
             return []
